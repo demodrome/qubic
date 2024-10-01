39e0285b0f577b12ddac3536035b610af720c8c2
Chris <167183247+demodrome@users.noreply.github.com>
2024-10-01 03:16:33 +0100

    fix: Fix bug causing tooltips to always render at the top of the document

--- scripts/ui/tooltips.js
+++ scripts/ui/tooltips.js
@@ -25,7 +25,7 @@ const tooltips = (function() {
           x = event.clientX - 10 - tooltip.offsetWidth;
         }
 
-        if (y <= window.innerHeight) {
+        if (y < 0) {
           y = 0;
         }
 
